930f0416c62dc1c2496a5da1a863ffa5e0a0d8a0
David <david.vidal@tecnativa.com>
2024-04-24 10:39:36 +0200

    [ADD] stock_weighing_threaded_print: New module
    TT47134

---
+++ stock_weighing_threaded_print/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2024 Tecnativa - David Vidal
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Weighing deferred label printing",
+    "summary": "Print labels on a different thread",
+    "version": "15.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Warehouse",
+    "website": "https://github.com/OCA/stock-weighing",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": [
+        "stock_weighing",
+        "base_report_to_printer",
+        "web_notify",
+    ],
+    "data": [],
+    "installable": True,
+}
