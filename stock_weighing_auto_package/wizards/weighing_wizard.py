05e20fb224508ebfa7aa318cf9307eeba0130bf4
sergio-teruel <sergio.teruel@tecnativa.com>
2024-11-04 10:24:11 +0100

    [FIX] stock_weighing_auto_package: Store stock_weighing_auto_package with sudo TT51569

--- stock_weighing_auto_package/wizards/weighing_wizard.py
+++ stock_weighing_auto_package/wizards/weighing_wizard.py
@@ -1,16 +1,27 @@
 # Copyright 2024 Tecnativa - Sergio Teruel
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class WeighingWizard(models.TransientModel):
     _inherit = "weighing.wizard"
 
     stock_weighing_auto_package = fields.Boolean(
-        related="product_id.stock_weighing_auto_package",
-        readonly=False,
+        compute="_compute_stock_weighing_auto_package",
+        inverse="_inverse_stock_weighing_auto_package",
         string="Auto package",
     )
+
+    @api.depends("product_id")
+    def _compute_stock_weighing_auto_package(self):
+        for wiz in self:
+            wiz.stock_weighing_auto_package = wiz.product_id.stock_weighing_auto_package
+
+    def _inverse_stock_weighing_auto_package(self):
+        for wiz in self:
+            wiz.product_id.sudo().stock_weighing_auto_package = (
+                wiz.stock_weighing_auto_package
+            )
 
     def _post_add_detailed_operation(self):
         res = super(WeighingWizard, self)._post_add_detailed_operation()
