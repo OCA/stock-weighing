b6e18eea3bc8724b9c24a96ae2a03e299eafa6a0
sergio-teruel <sergio.teruel@tecnativa.com>
2025-04-03 23:05:08 +0200

    [IMP] web_widget_remote_measure: Add button to test connection with the device TT55802

--- web_widget_remote_measure/models/remote_measure_device.py
+++ web_widget_remote_measure/models/remote_measure_device.py
@@ -1,6 +1,11 @@
 # Copyright 2023 Tecnativa - David Vidal
+# Copyright 2025 Tecnativa - Sergio Teruel
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import fields, models
+import socket
+from contextlib import closing
+from urllib.parse import urlparse
+
+from odoo import _, fields, models
 
 
 class RemoteMeasureDevice(models.Model):
@@ -38,3 +43,35 @@ class RemoteMeasureDevice(models.Model):
         help="(0 for no sleep between reads) Miliseconds to wait between device reads"
     )
     test_measure = fields.Float(default=0.0)
+
+    def test_tcp_connection(self):
+        """
+        Verify the TCP connection with the remote device
+        """
+        parsed_address = urlparse(f"//{self.host}")
+        host = parsed_address.hostname
+        port = parsed_address.port
+        timeout = 2.0
+        try:
+            with closing(socket.create_connection((host, port), timeout)):
+                return self.test_connection_notification("success", host, port)
+        except (socket.timeout, socket.error):
+            return self.test_connection_notification("danger", host, port)
+
+    def test_connection_notification(self, notification_type, host, port):
+        if notification_type == "success":
+            message = _(
+                "Successfully connected to %(host)s:%(port)s", host=host, port=port
+            )
+        else:
+            message = _("Connection failure to %(host)s:%(port)s", host=host, port=port)
+        return {
+            "type": "ir.actions.client",
+            "tag": "display_notification",
+            "params": {
+                "title": _("Test connection"),
+                "message": message,
+                "sticky": False,
+                "type": notification_type,
+            },
+        }
