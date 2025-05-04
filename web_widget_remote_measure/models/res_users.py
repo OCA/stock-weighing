3ebb73e068fa3f0027b3d7163de4f193d5591693
sergio-teruel <sergio.teruel@tecnativa.com>
2025-05-04 08:35:53 +0200

    [FIX] web_widget_remote_measure: Users without setting security group can not write in your own user

--- web_widget_remote_measure/models/res_users.py
+++ web_widget_remote_measure/models/res_users.py
@@ -1,5 +1,6 @@
 # Copyright 2023 Tecnativa - David Vidal
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2025 Tecnativa - Sergio Teruel
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo import fields, models
 
 
@@ -15,3 +16,14 @@ class ResUsers(models.Model):
         return {
             "type": "ir.actions.act_window_close",
         }
+
+    # Allow users without setting security group read and write the remote device
+    @property
+    def SELF_READABLE_FIELDS(self):
+        return super().SELF_READABLE_FIELDS + ["remote_measure_device_id"]
+
+    @property
+    def SELF_WRITEABLE_FIELDS(self):
+        return super().SELF_WRITEABLE_FIELDS + [
+            "remote_measure_device_id",
+        ]
