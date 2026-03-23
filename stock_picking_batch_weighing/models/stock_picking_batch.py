e0e0ab39229fbfc0360ebd5b0d1d435d57a4e835
Andrii9090-tecnativa <andrii.kompaniiets@tecnativa.com>
2026-03-23 15:53:08 +0100

    [IMP] stock_picking_batch_weighing: pre-commit auto fixes

--- stock_picking_batch_weighing/models/stock_picking_batch.py
+++ stock_picking_batch_weighing/models/stock_picking_batch.py
@@ -38,6 +38,6 @@ class StockPickingBatch(models.Model):
         action["context"] = dict(
             self.env.context,
             **ast.literal_eval(action["context"]),
-            group_by=["picking_id"]
+            group_by=["picking_id"],
         )
         return action
