9d175a1a6febe88f3185126f892798503998840f
Andrii9090-tecnativa <andrii.kompaniiets@tecnativa.com>
2026-03-25 11:28:43 +0100

    [MIG] stock_secondary_unit_weighing: Migration to 18.0

--- stock_secondary_unit_weighing/__manifest__.py
+++ stock_secondary_unit_weighing/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Weighing assistant and secondary units",
     "summary": "Show secondary unit info in the weighing assistant",
-    "version": "15.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-weighing",
     "license": "AGPL-3",
