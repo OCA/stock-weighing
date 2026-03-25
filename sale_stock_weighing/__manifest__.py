e8486f3701a18c8fe776ebad670385fe56c6f0fb
Andrii9090-tecnativa <andrii.kompaniiets@tecnativa.com>
2026-03-25 13:24:36 +0100

    [MIG] sale_stock_weighing: Migration to 18.0

--- sale_stock_weighing/__manifest__.py
+++ sale_stock_weighing/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Weighing assistant sales info",
     "summary": "Weight assistant extension sales",
-    "version": "15.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-weighing",
     "license": "AGPL-3",
