OCA/sale-workflow

[ADD] sale_stock_weighing: New module
TT47127

sale_stock_weighing/__manifest__.py
@@ -1,0 +1,17 @@
+# Copyright 2024 Tecnativa - David Vidal
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Weighing assistant sales info",
+    "summary": "Weight assistant extension sales",
+    "version": "15.0.1.0.0",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow",
+    "license": "AGPL-3",
+    "category": "Inventory",
+    "depends": [
+        "stock_weighing",
+        "sale_stock",
+    ],
+    "data": ["views/stock_move_views.xml"],
+    "installable": True,
+}

OCA/stock-weighing

sale_stock_weighing/__manifest__.py
@@ -1,0 +1,17 @@
+# Copyright 2024 Tecnativa - David Vidal
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Weighing assistant sales info",
+    "summary": "Weight assistant extension sales",
+    "version": "15.0.1.0.0",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-weighing",
+    "license": "AGPL-3",
+    "category": "Inventory",
+    "depends": [
+        "stock_weighing",
+        "sale_stock",
+    ],
+    "data": ["views/stock_move_views.xml"],
+    "installable": True,
+}