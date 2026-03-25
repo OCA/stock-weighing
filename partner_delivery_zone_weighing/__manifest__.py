8fd157af018086ed2ad1123f502cb652bdfdac98
Andrii9090-tecnativa <andrii.kompaniiets@tecnativa.com>
2026-03-25 12:20:58 +0100

    [MIG] partner_delivery_zone_weighing: Migration to 18.0

--- partner_delivery_zone_weighing/__manifest__.py
+++ partner_delivery_zone_weighing/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Weighing assistant with delivery zones",
     "summary": "Show delivery zones info in weighings",
-    "version": "15.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-weighing",
     "license": "AGPL-3",
