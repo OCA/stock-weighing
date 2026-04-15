2f98ede2784d0e6bb370f9d276bc33d778961141
Andrii9090-tecnativa <andrii.kompaniiets@tecnativa.com>
2026-04-15 09:33:19 +0200

    [MIG] mrp_weighing_picking_batch: Migration to 18.0

--- mrp_weighing_picking_batch/__manifest__.py
+++ mrp_weighing_picking_batch/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Validate batch pickings and close MRP",
     "summary": "Close MRP from batch picking",
-    "version": "15.0.1.0.1",
+    "version": "18.0.1.0.0",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-weighing",
     "license": "AGPL-3",
