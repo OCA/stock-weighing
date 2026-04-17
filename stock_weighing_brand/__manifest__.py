d44622b81f6eaa223d45f3e7d656691b9c6dcb6d
Andrii9090-tecnativa <andrii.kompaniiets@tecnativa.com>
2026-04-17 11:06:08 +0200

    [MIG] stock_weighing_brand: Migration to 18.0

--- stock_weighing_brand/__manifest__.py
+++ stock_weighing_brand/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Show brand logo in Weighing assistant",
     "summary": "Show product logo in Weighing assistant",
-    "version": "15.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-weighing",
     "license": "AGPL-3",
