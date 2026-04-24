4a31e91f054d2e553002bc9be388c23da1977737
Andrii9090-tecnativa <andrii.kompaniiets@tecnativa.com>
2026-04-24 13:30:08 +0200

    [FIX] stock_weighing: Add Interwarehouse to start screen

---
+++ stock_weighing/migrations/18.0.1.0.8/post-migrate.py
@@ -0,0 +1,11 @@
+# Copyright 2026 Tecnativa - Andrii Kompaniiets
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.load_data(
+        env, "stock_weighing", "migrations/18.0.1.0.8/noupdate_changes.xml"
+    )
